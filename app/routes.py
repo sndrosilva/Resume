bcc06eff6391fe442fc5a4e33c0684e0462b9574
Sandro Silva <silva.sandroluis@gmail.com>
2020-08-13 12:52:01 +0800

    fixed navbar

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,8 @@
 from app import app
-from flask import render_template
+from flask import render_template, request, send_from_directory
 
 @app.route('/')
 @app.route('/index')
 def index():
     return render_template('index.html', title='Home')
+
